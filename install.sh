c9d11158ea3e9fcd19175dc36a1feaa823f3c569
SuppGalaxtyca <90016600+SuppGalaxtyca@users.noreply.github.com>
2021-09-06 07:19:41 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+apt-get update
+apt-get upgrade
+apt-get install nodejs
+apt-get install libwebp
+apt-get install mc
+apt-get install ffmpeg
+apt-get install wget
+apt-get install tesseract
+wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
+npm install
+
+echo "⟨--------------------------------------------------⟩"
+echo "sukses data telah di install semua, tinggal ketik npm start enter"
+echo "scrip ini jangan di jual /scrip ini free buat kalian semua"
+echo "SUBS YT DBOT"
+echo "KLO EROR HUBUNGI AJA OWN"
+echo "SUBS YO..YO"
+echo "⟨--------------------------------------------------⟩"
